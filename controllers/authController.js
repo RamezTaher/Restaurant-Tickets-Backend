e6b370b2fd2ba4c915c8ac2509b82cf147035d52
Ramez Taher <ramezgamer11@gmail.com>
2022-08-30 12:18:12 +0100

    auth signup added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,31 @@
+const bcrypt = require("bcryptjs")
+const User = require("../models/userModel")
+
 const signUpUser = async (req, res) => {
   try {
     const { name, email, password } = req.body
+
+    const userExists = await User.findOne({ email })
+    if (userExists) {
+      return res.status(422).json("Email already exist")
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hanshedPassword = await bcrypt.hash(password, salt)
+
+    const user = await User.create({
+      name,
+      email,
+      password: hanshedPassword,
+    })
+
+    if (user) {
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+      })
+    }
 
     res.send("Sign up")
   } catch (err) {
---
+++ models/userModel.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+    },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+)
+
+module.exports = mongoose.model("User", UserSchema)

dfc5316e00205948083a89c7ed10431abf36b9ab
Ramez Taher <ramezgamer11@gmail.com>
2022-09-03 06:04:21 +0100

    controllers added

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -3,10 +3,39 @@ const Order = require("../models/orderModel")
 
 const getOrders = async (req, res) => {
   try {
-  } catch (error) {}
+    const user = await User.findById(req.user.id)
+
+    if (!user) {
+      return res.status(401).json("User not found")
+    }
+
+    const orders = await Order.find({ user: req.user.id })
+    res.status(200).json(orders)
+  } catch (error) {
+    return res.status(404).json(error)
+  }
 }
 const createOrder = async (req, res) => {
   try {
+    const { order, description } = req.body
+
+    if (!order || !description) {
+      return res.status(400).json("Please add Product and description")
+    }
+
+    const user = await User.findById(req.user.id)
+    console.log(req.user.id)
+    if (!user) {
+      return res.status(401).json("User not found")
+    }
+
+    const orders = await Order.create({
+      order,
+      description,
+      user: req.user.id,
+      status: "new",
+    })
+    res.status(201).json(orders)
   } catch (error) {}
 }
 

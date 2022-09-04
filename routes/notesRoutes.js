7a9c953593949bcdf5c84675ad125bda45427acc
Ramez Taher <ramezgamer11@gmail.com>
2022-09-05 00:58:20 +0100

    notes route

--- routes/notesRoutes.js
+++ routes/notesRoutes.js
@@ -1,4 +1,6 @@
 const express = require("express")
 const router = express.Router()
-
 const { protect } = require("../middleware/authMiddleware")
+const { getNotes } = require("../controllers/notesController")
+
+route.router("/").get(protect, getNotes)
